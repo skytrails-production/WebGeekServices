a2a2c114cc5ceb73f57a56b5c4834e249cea633d
armaan <armaanalam65@gmail.com>
2025-08-12 22:35:33 +0530

    remove image

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class TechnicalAssistance(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
 class ConnectNow(models.Model):
-    image = models.ImageField(upload_to='ConnectNow',null=True,blank=True)
+    
     title = models.CharField(max_length=255)
     link = models.URLField(max_length=500,null=True,blank=True)
 
